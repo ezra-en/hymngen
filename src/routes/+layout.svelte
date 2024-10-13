a13f41e9ec44df4b26be477c5e0549272bcecec4
EzraEn <7076802+ezra-en@users.noreply.github.com>
2024-10-13 19:22:52 +0800

    feat: analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,8 @@
 	import { Toaster } from '$lib/components/ui/sonner';
 	import '../app.pcss';
   import { ModeWatcher } from "mode-watcher";
+	// @ts-ignore
+	import { inject } from '@vercel/analytics'
 </script>
  
 <ModeWatcher />
